f8dc43992ff53be383a7d9eca46509896e5ad254
Jongwoo Lee <jongwoo@jongwoo.dev>
2025-07-09 22:38:20 +0900

    Prepare for hosting

--- game.js
+++
@@ -1,9 +0,0 @@
-// Initialize the path drawer
-const startPoint = { x: 100, y: 200 };
-const endPoint = { x: 700, y: 400 };
-const pathDrawer = new PathDrawer(canvas, world, startPoint, endPoint);
-
-// Add a clear button functionality
-function clearPath() {
-    pathDrawer.clearPath();
-}
--- main.js
+++ main.js
@@ -155,25 +155,25 @@ function timeSince(date) {
   var interval = seconds / 31536000;
 
   if (interval > 1) {
-    return Math.floor(interval) + "년";
+    return Math.floor(interval) + "년 전";
   }
   interval = seconds / 2592000;
   if (interval > 1) {
-    return Math.floor(interval) + "개월";
+    return Math.floor(interval) + "개월 전";
   }
   interval = seconds / 86400;
   if (interval > 1) {
-    return Math.floor(interval) + "일";
+    return Math.floor(interval) + "일 전";
   }
   interval = seconds / 3600;
   if (interval > 1) {
-    return Math.floor(interval) + "시간";
+    return Math.floor(interval) + "시간 전";
   }
   interval = seconds / 60;
   if (interval > 1) {
-    return Math.floor(interval) + "분";
+    return Math.floor(interval) + "분 전";
   }
-  return Math.floor(seconds) + "초";
+  return Math.floor(seconds) + "초 전";
 }
 
 // 리더보드 업데이트
